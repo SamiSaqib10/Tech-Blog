6f1cc7763db85b3bca1b5616e9e35148de1627bb
Sami Ur Rehman Saqib <samisaqib10@gmail.com>
2023-07-09 21:49:47 -0400

    fixed bug

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models/Index');
+const { User, Post, Comment } = require('../../models/index');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
